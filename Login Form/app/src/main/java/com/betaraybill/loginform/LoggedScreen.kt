1477e4105413c5d3427cb8215fa4cbd0d96a08d4
Betaraybill <raijinthegodofthunder.com>
2019-12-26 19:19:13 +0200

    forgor the files

--- Login Form/app/src/main/java/com/betaraybill/loginform/LoggedScreen.kt
+++ Login Form/app/src/main/java/com/betaraybill/loginform/LoggedScreen.kt
@@ -2,11 +2,17 @@ package com.betaraybill.loginform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class LoggedScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged_screen)
+        val email = findViewById<TextView>(R.id.emailTextView)
+        val password = findViewById<TextView>(R.id.passwordTextView)
+        email.text = "Email: ${intent.getStringExtra("email")}"
+        password.text = "Password: ${intent.getStringExtra("password")}"
     }
 }
--- Login Form/app/src/main/java/com/betaraybill/loginform/MainActivity.kt
+++ Login Form/app/src/main/java/com/betaraybill/loginform/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.betaraybill.loginform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = findViewById<EditText>(R.id.emailField).text
+        val password = findViewById<EditText>(R.id.passwordField).text
+        val buttonA = findViewById<Button>(R.id.loginButton)
+        buttonA.setOnClickListener {
+            if (emailIsValid(email.toString())) {
+                val intent = Intent(this, LoggedScreen::class.java)
+                intent.putExtra("email", email.toString())
+                intent.putExtra("password", password.toString())
+                startActivity(intent)
+            } else {
+                Toast.makeText(this@MainActivity, "Invalid email", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun emailIsValid(email: String) : Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
