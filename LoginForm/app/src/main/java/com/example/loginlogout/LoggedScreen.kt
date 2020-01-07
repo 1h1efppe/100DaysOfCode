ca14656d1ea2e0a05ee350bf2178e48d42b01530
betaraybill <raijinthegodofthunder.com>
2020-01-07 22:30:28 +0200

    day 14 challenge - done

---
+++ LoginForm/app/src/main/java/com/example/loginlogout/LoggedScreen.kt
@@ -0,0 +1,12 @@
+package com.example.loginlogout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoggedScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged_screen)
+    }
+}
